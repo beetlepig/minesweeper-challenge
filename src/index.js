ec73c7aa5960278f354e64da1b79cc6db657dcb4
karlos <sky_karlos@hotmail.com>
2018-12-05 23:23:10 -0500

    user commands

--- src/index.js
+++ src/index.js
@@ -6,32 +6,79 @@ const rl = readLine.createInterface({
 	terminal: true
 });
 
-async function init() {
-	try {
-		await askForInitialParameters();
-	} catch (e) {
+function init() {
+
+	askForInitialParameters().then( gameConfiguration => {
+		listenForUserSubmit(gameConfiguration);
+	}).catch(e => {
 		process.stdout.write(`${e}\n`);
 		process.stdout.write('Retying...');
 		setTimeout(() => {
 			init();
 		}, 5000);
-	}
+	});
+
+}
+
+function listenForUserSubmit (gameConfiguration) {
+
+	rl.on('line', (command) => {
+		process.stdout.write('\u001B[2J\u001B[0;0f');
+
+		checkUserCommand(gameConfiguration, command).then((f) => {
+			process.stdout.write( `${f}\n`);
+		}).catch((r) => {
+			process.stdout.write( `${r}\n`);
+		});
+
+	});
+}
+
+function checkUserCommand(gameConfiguration, command) {
+	const {width, height} = gameConfiguration;
+	const commandPattern = /^([0-9]{1,2},[0-9]{1,2},[UM])$/;
+
+	return new Promise((resolve, reject) =>{
+
+		const commandFixedArray = command.trim().split(' ').map((char, index) => {
+			if (index < 2) {
+				return parseInt(char);
+			} else {
+				return char;
+			}
+		});
+
+		const commandFixedString = commandFixedArray.toString();
+
+		if (commandPattern.test(commandFixedString)) {
+			if (commandFixedArray[0] > width || commandFixedArray[1] > height) {
+				reject('invalid coordinates');
+			} else {
+				resolve('valid command');
+			}
+		} else {
+			reject('invalid command');
+		}
+
+		process.stdout.write( `user input: ${JSON.stringify(commandFixedString)}\n`);
+	});
 }
 
 async function askForInitialParameters() {
-	let width, height, minesNumber;
+	let width = 5, height = 5, minesNumber = 5;
 	try {
 		await promptForBoardWidth().then((_width) => {
-			width = _width;
+			width = parseInt(_width);
 		});
 
 		await promptForBoardHeight(width).then((_height) => {
-			height = _height;
+			height = parseInt(_height);
 		});
 
 		await  promptForMinesNumber(width, height).then((_mines) => {
-			minesNumber = _mines;
+			minesNumber = parseInt(_mines);
 		});
+		return Promise.resolve({width, height, minesNumber});
 	} catch (e) {
 		return Promise.reject(new Error(e));
 	}
@@ -43,7 +90,7 @@ function promptForBoardWidth() {
 		process.stdout.write('This is the Karlos version of Minesweeper, running in Nodejs \n');
 		rl.question('How many Columns do you want the board to have? Type a number between 5 and 15\n', answer => {
 			if (answer >= 5 && answer <= 15) {
-				resolve(answer);
+				resolve(Math.floor(parseInt(answer)));
 			} else {
 				reject('This seems to be a invalid input, make sure that you enter a number between the requested width range :p\n');
 			}
@@ -57,7 +104,7 @@ function promptForBoardHeight(width) {
 		process.stdout.write( `Columns: ${width}\n`);
 		rl.question('How many Rows do you want the board to have? Type a number between 5 and 15\n', answer => {
 			if (answer >= 5 && answer <= 15) {
-				resolve(answer);
+				resolve(Math.floor(parseInt(answer)));
 			} else {
 				reject('This seems to be a invalid input, make sure that you enter a number between the requested height range :p\n');
 			}
@@ -72,7 +119,7 @@ function promptForMinesNumber(width, height) {
 		process.stdout.write( `Columns: ${width}  Rows: ${height}\n`);
 		rl.question(`How many Mines do you want the board to have? Type a number between 1 and ${maxMinesNumber}\n`, answer => {
 			if (answer >= 1 && answer <= maxMinesNumber) {
-				resolve(answer);
+				resolve(Math.floor(parseInt(answer)));
 			} else {
 				reject('This seems to be a invalid input, make sure that you enter a number between the requested Mines range :p\n');
 			}
